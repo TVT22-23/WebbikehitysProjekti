8a2c39a0fe6c0782119f84c0f26897e372f97f52
Edilainen <eetu.luhtala@gmail.com>
2023-11-20 11:41:52 +0200

    Added group functionality to database

--- postgre/movieGroup.js
+++ postgre/movieGroup.js
@@ -1,4 +1,21 @@
 const pgPool = require('./connection');
 
+const sql = {
+    INSERT_GROUP: 'INSERT INTO moviegroup (group_name, description, owner) VALUES ($1, $2, $3)',
+    GET_GROUP: 'SELECT group_name, description, owner FROM moviegroup'
+}
 
-module.exports = {, };
+async function addGroup(group_name, description, owner) {
+    await pgPool.query(sql.INSERT_GROUP, [group_name, description, owner])
+}
+
+addGroup('testGroup', 'testDescription', '1');
+getGroup();
+
+async function getGroup(){
+    const result = await pgPool.query(sql.GET_GROUP);
+    const rows = result.rows;
+    return rows;
+}
+
+module.exports = {addGroup, getGroup};
