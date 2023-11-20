4bb1c7012daf2262378d2ee4e60f94b8c3413a95
Retrobitti <PekkaParviainen@protonmail.com>
2023-11-20 11:33:37 +0200

    Added review functionality to database
    added review.js and reviewRoute.js

---
+++ Routes/reviewRoute.js
@@ -0,0 +1,28 @@
+const router = require('express').Router();
+const multer = require('multer');
+const upload = multer({dest: 'upload/'});
+
+const {addReview, getReview} = require('../postgre/review');
+
+router.get('/review_id', async (req, res) => {
+
+        res.json(await getReview());
+});
+
+router.post('/review', upload.none() , async (req, res) => {
+    const text_review = req.body.text_review;
+    const rating = req.body.rating;
+    const recommend = req.body.recommend;
+
+
+
+    try {
+        await addReview(text_review, rating, recommend);
+        res.end();
+    } catch (error) {
+        console.log(error);
+        res.json({error: error.message}).status(500);
+    }
+});
+
+module.exports = router;
--- postgre/review.js
+++ postgre/review.js
@@ -1,4 +1,22 @@
 const pgPool = require('./connection');
 
+const sql = {
+    INSERT_REVIEW: 'INSERT INTO review (text_review, rating, recommend) VALUES ($1, $2, $3)',
+    GET_REVIEW: 'SELECT * FROM review'
+}
 
-module.exports = {, };
+//addReview('katsomattaHuono', '3', 'false');
+getReview();
+
+
+async function addReview(text_review, rating, recommend) {
+    await pgPool.query(sql.INSERT_REVIEW, [text_review, rating, recommend])
+}
+
+async function getReview(){
+    const result = await pgPool.query(sql.GET_REVIEW);
+    const rows = result.rows;
+    return rows;
+}
+
+module.exports = {addReview, getReview};
