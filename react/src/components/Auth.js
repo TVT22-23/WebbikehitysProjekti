c3d9f01985d1f72fc7329500398539047ab6834f
Hajax8 <t2luha01@students.oamk.fi>
2023-11-20 13:21:58 +0200

    Routes updated

--- react/src/components/Auth.js
+++ react/src/components/Auth.js
@@ -1,17 +1,18 @@
 import { useContext } from "react";
-import { LoginContext } from "./Contexts";
+import { LoginContext} from "./Contexts"
 
 function Login() {
-    const { login, setLogin } = useContext(LoginContext);
+    // const { login, setLogin } = useContext(LoginContext);
 
-    const buttonLabel = login ? 'logout' : 'login';
+    // const buttonLabel = login ? 'logout' : 'login';
 
     return (
         <div>
-            {login ? <h2>Welcome</h2> : <h2>unauthorized</h2>}
-            <button onClick={() => setLogin(prev => !prev)}>{buttonLabel}</button>
+            <h1>login</h1>
+            {/* {login ? <h2>Welcome</h2> : <h2>unauthorized</h2>}
+            <button onClick={() => setLogin(prev => !prev)}>{buttonLabel}</button> */}
         </div>
     );
 }
 
-export { Login };
+export {Login};
---
+++ react/src/components/Film.js
@@ -0,0 +1,40 @@
+import { Link, Outlet, useParams } from "react-router-dom";
+
+function Film() {
+
+  const { filmID } = useParams();
+
+  return (
+    <div>
+      <h1>Here is so much info about the film with id {filmID}</h1>
+       <Link to={'cast'}>Cast  </Link>
+        <Link to={'crew'}>Crew </Link>
+        <Outlet/>
+    </div>
+  )
+}
+
+function Cast (){
+  return(
+    <div>
+      <h4>People who acted in the movie</h4>
+      <li>Kasper</li>
+      <li>Jesper</li>
+      <li>Niko</li>
+    </div>
+  )
+}
+
+function Crew (){
+  return(
+    <div>
+      <h4>People Who made the movie</h4>
+      <li>MAtti</li>
+      <li>Jorma</li>
+      <li>Jaska</li>
+    </div>
+  )
+}
+
+
+export {Film, Crew, Cast};
