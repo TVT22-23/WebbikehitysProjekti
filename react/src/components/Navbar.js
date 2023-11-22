4a516609373f5a3f58bf8d69b09f13cdf8fca119
Lauri Tuovinen <lare.tuovinen@gmail.com>
2023-11-22 15:41:11 +0200

    fuck it new navbar
    suck it

--- react/src/components/Navbar.js
+++ react/src/components/Navbar.js
@@ -1,46 +1,23 @@
-import {Link} from 'react-router-dom';
 import { useContext } from "react";
 
 function Navbar() {
 
 return(
-<nav class="navbar navbar-inverse">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="#">terve terve terva perse</a>
-    </div>
-    <ul class="nav navbar-nav">
-      <li class="navbar">
-      <Link to={''} className='nav-link'>Home</Link>
-      </li>
-      <li>
-      <Link to={'/user'} className='nav-link'>User</Link>
-      </li>
-      <li>
-      <Link to={'/auth'} className='nav-link'>Login</Link>
-      </li>
-      <li>
-      <Link to={'/film'} className='nav-link'>Film</Link>
-      </li>
-      <li>
-      <Link to={'/group'} className='nav-link'>Group</Link>
-      </li>
-      <li>
-      <Link to={'/settings'} className='nav-link'>Settings</Link>
-      </li>    
-    </ul>
-<form class="form-inline">
-      <div class="input-group">
-        <input type="text" class="form-control" placeholder="Search" name="search"></input>
-        <div class="input-group-btn">
-          <button class="btn btn-default" type="submit">
-            <i class="glyphicon glyphicon-search"></i>
-          </button>
-        </div>
-      </div>
+<div class="topnav">
+  <a class="active" href="">Home</a>
+  <a href="user">User</a>
+  <a href="login">Login</a>
+    <a href="film">Film</a>
+  <a href="group">Group</a>
+  <a href="settings">Settings</a>
+
+  <div class="search-container">
+    <form action="/action_page.php">
+      <input type="text" placeholder="Search.." name="search"></input>
+      <button type="submit">search</button>
     </form>
-    </div>
-</nav>
+  </div>
+</div>
     );
 }
 export { Navbar };
