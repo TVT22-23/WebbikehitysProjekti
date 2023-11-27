c4448c4beadace33be40db3c9af20b6a403cfbdd
Edilainen <eetu.luhtala@gmail.com>
2023-11-27 13:31:21 +0200

    Added DELETE endpoint to member

--- Routes/memberRoute.js
+++ Routes/memberRoute.js
@@ -3,19 +3,29 @@ const multer = require('multer');
 const upload = multer({dest: 'upload/'});
 
 
-const {addMember, getMember} = require('../postgre/member');
+const {addMember, getMember, deleteMember} = require('../postgre/member');
 
 router.get('/', async (req, res) => {
 
         res.json(await getMember());
 });
 
-router.post('/', upload.none() , async (req, res) => {
+router.post('/create', upload.none() , async (req, res) => {
     const account_accountid = req.body.account_accountid;
     const group_groupid = req.body.group_groupid;
 
     try {
         await addMember(account_accountid, group_groupid);
+        res.end();
+    } catch (error) {
+        console.log(error);
+        res.json({error: error.message}).status(500);
+    }
+});
+
+router.delete('/delete/:member_id', upload.none() , async (req, res) => {
+    try {
+        await deleteMember(req.params.member_id);
         res.end();
     } catch (error) {
         console.log(error);
--- postgre/member.js
+++ postgre/member.js
@@ -2,7 +2,8 @@ const pgPool = require('./connection');
 
 const sql = {
     INSERT_MEMBER: 'INSERT INTO member (account_accountid, group_groupid) VALUES ($1, $2)',
-    GET_MEMBER: 'SELECT account_id, group_id FROM member JOIN account ON account_accountid = account.account_id JOIN moviegroup ON group_groupid = moviegroup.group_id'
+    GET_MEMBER: 'SELECT account_id, group_id FROM member JOIN account ON account_accountid = account.account_id JOIN moviegroup ON group_groupid = moviegroup.group_id',
+    DELETE_MEMBER: 'DELETE FROM member WHERE member_id=$1'
 }
 
 //addMember('1', '1');
@@ -19,4 +20,8 @@ async function getMember(){
     return rows;
 }
 
-module.exports = {addMember, getMember};
+async function deleteMember(member_id) {
+    await pgPool.query(sql.DELETE_MEMBER, [member_id]);
+}
+
+module.exports = {addMember, getMember, deleteMember};
