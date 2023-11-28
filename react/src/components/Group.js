1f0b3092a5daa403433357bd9957c92165d03e15
Lauri Tuovinen <lare.tuovinen@gmail.com>
2023-11-28 11:55:20 +0200

    started working on group page
    looks good, but not good enough

--- react/src/components/Group.js
+++ react/src/components/Group.js
@@ -1,12 +1,34 @@
 import { useParams } from "react-router-dom";
+import { Col, Container, Image, Row, Form, Card } from 'react-bootstrap';
+import { NewsGrid, ReviewGrid } from "./Home";
 
 function Group() {
 
-    const {groupID} = useParams();
 
     return (
+       <div>
+          <Row> 
+            <h1>Group with id </h1>
+            <GroupName />  
+          </Row>
+          <Row>
+          <h3 className="my-4" style={{ color: '#CA3e47', borderBottom: '1px solid #CA3E47' }}>Saved reviews</h3>
+            <NewsGrid />
+            <NewsGrid />
+          </Row>
+          <Row>
+          <h3 className="my-4" style={{ color: '#CA3e47', borderBottom: '1px solid #CA3E47' }}>News</h3>
+            <ReviewGrid />
+          </Row>
+        </div>
+    )
+}
+
+function GroupName() {
+    const {groupID} = useParams();
+    return(
         <div>
-            <h1>Group with id {groupID}</h1>
+            <h class="groupBanner"> Group {groupID} </h>
         </div>
     )
 }
