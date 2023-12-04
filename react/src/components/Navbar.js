ed1f7f4b29bf57a42357b2f78ea72c575710c547
Retrobitti <PekkaParviainen@protonmail.com>
2023-12-04 16:38:26 +0200

    Added functionality

--- react/src/components/Navbar.js
+++ react/src/components/Navbar.js
@@ -5,21 +5,21 @@ function Navbar() {
 return(
 <div className="topnav">
   
-  {/* <Link to="/" className="active">Home</Link>
+  <Link to="/" className="active">Home</Link>
   <Link to="/user">User</Link>
   <Link to="/film">Films</Link>
   <Link to="/group">Group</Link>
   <Link to="/settings">Settings</Link>
   <Link to="/login">Login</Link>
-  <Link to="/searchFilms">Search Films</Link> */}
+  <Link to="/searchFilms">Search Films</Link>
 
-  <a className="active" href="/" >Home</a>
+  {/*<a className="active" href="/" >Home</a>
    <a href="user">User</a>
   <a href="film">Films</a>
   <a href="groups">Groups</a>
   <a href="settings">Settings</a>
   <a href="login">Login</a>
-  <a href="searchFilms">Search Films</a>
+  <a href="searchFilms">Search Films</a>*/}
 
   <div className="search-container">
     <form action="/action_page.php">
