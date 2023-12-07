14e6c229e477bd8c11e69bccca8bea16aa72ba7b
Hajax8 <t2luha01@students.oamk.fi>
2023-12-07 12:28:30 +0200

    SearchFilms page selection
    When discovering movies with genres, show buttons to change page

--- react/src/components/SearchFilms.js
+++ react/src/components/SearchFilms.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
-import { Row, Image, Dropdown, Col } from "react-bootstrap";
+import { Row, Image, Dropdown, Col, Pagination } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 
 function SearchFilms() {
@@ -10,9 +10,11 @@ function SearchFilms() {
     const [genres, setGenres] = useState([]);
     const navigate = useNavigate();
     const [otsikko, setOtsikko] = useState('');
-
+    const [CurrentPage, setCurrentPage] = useState(1);
+    const [totalPages, setTotalPages] = useState('');
 
     const getMovies = (url) => {
+        console.log(url);
         fetch(url)
             .then(res => res.json())
             .then(data => {
@@ -24,29 +26,33 @@ function SearchFilms() {
                 }));
 
                 setFilms(updatedFilms);
+                //Set CurrentPage and total pages to be used.
+                axios.get(url).then(resp => setCurrentPage(resp.data.page));
+                axios.get(url).then(resp => setTotalPages(resp.data.total_pages));
             })
             .catch(error => {
                 console.error('Error fetching movies:', error);
             });
     };
+
     //Search movies with search input. If no search input, fetch by genre and order, else show trending movies.
     useEffect(() => {
-        console.log(genres);
         if (searchInput.trim() === '') {
             if (sortBy !== '' || genres.length > 0) {
                 // Fetch movies based on genre and order
                 const filterUrl = `https://api.themoviedb.org/3/discover/movie?sort_by=${encodeURIComponent(sortBy)}&with_genres=${encodeURIComponent(genres)}&api_key=3972673c7c2bf3c70fc1b5593e956b47`;
-                getMovies(filterUrl);
+                // Add &page= and currentPage to url end.
+                getMovies(filterUrl + '&page=' + encodeURI(CurrentPage));
             } else {
                 // Fetch trending movies
                 axios.get("https://api.themoviedb.org/3/trending/movie/week?api_key=3972673c7c2bf3c70fc1b5593e956b47")
-                    .then(resp => 
-                         setFilms(resp.data.results.map(f => ({
-                        Rating: f.vote_average,
-                        movieID: f.id,
-                        Title: f.title,
-                        Poster: f.poster_path
-                    }))))
+                    .then(resp =>
+                        setFilms(resp.data.results.map(f => ({
+                            Rating: f.vote_average,
+                            movieID: f.id,
+                            Title: f.title,
+                            Poster: f.poster_path
+                        }))))
                     .catch(error => {
                         console.error('Error fetching data:', error);
                     });
@@ -56,7 +62,9 @@ function SearchFilms() {
             const searchUrl = `https://api.themoviedb.org/3/search/movie?api_key=3972673c7c2bf3c70fc1b5593e956b47&query=${encodeURIComponent(searchInput)}`;
             getMovies(searchUrl);
         }
-    }, [searchInput, genres, sortBy]);
+
+
+    }, [searchInput, genres, sortBy, CurrentPage]);
 
     const handleMovieClick = (movieID) => {
         // Use navigate to go to the Film component with the clicked movieID
@@ -66,18 +74,30 @@ function SearchFilms() {
     function handleSetSortBy(by, menuTitle) {
         setSortBy(by);
         setOtsikko(menuTitle);
+        setCurrentPage(1);
     };
     //Add selected genre to genres array, if it is there, it is removed. 
     //If genreId is "empty" empty the genres array
     function handleSetGenre(genreID) {
-        if(genreID === 'empty'){
+        if (genreID === 'empty') {
             setGenres([]);
         } else if (!genres.includes(genreID)) {
-            setGenres([...genres, genreID])
+            setGenres([...genres, genreID]);
+            setCurrentPage(1);
         } else {
-            setGenres(prev => prev.filter(id => id !== genreID))
+            setGenres(prev => prev.filter(id => id !== genreID));
+            setCurrentPage(1);
         };
+    };
 
+    function handleDecrementPage() {
+        setCurrentPage(CurrentPage - 1);
+        window.scrollTo(0, 0);
+    };
+
+    function handleIncrementPage() {
+        setCurrentPage(CurrentPage + 1);
+        window.scrollTo(0, 0);
     };
 
     return (
@@ -86,25 +106,26 @@ function SearchFilms() {
                 <Col lg={2}>
                 </Col>
                 <Col lg={4}>
-                    <h2 className="mt-3" style={{ color: '#Ca3e47' }}>Hae elokuvaa</h2>
-                    <p className="text-muted" style={{fontSize:'15px'}}>Etsi elokuvan nimellä tai valitse genre ja  esitysjärjestys</p>
-                    <input className="mb-2 w-75 rounded " type="search" placeholder="Hae elokuvia..." onChange={(e) => setSearchInput(e.target.value)} />
+                    <h2 className="mt-3" style={{ color: '#Ca3e47' }}>Search fo a movie</h2>
+                    <p className="text-muted" style={{ fontSize: '15px' }}>Search by name or by genre</p>
+                    <input className="mb-2 w-75 rounded " type="search" placeholder="Search..." onChange={(e) => setSearchInput(e.target.value)} />
                 </Col>
                 <Col>
                     {/* dropdown menu to choose how to sort films */}
                     <Dropdown>
                         <Dropdown.Toggle >
-                            Järjestä {otsikko}
+                            Order by {otsikko}
                         </Dropdown.Toggle>
 
                         <Dropdown.Menu>
                             <Dropdown.Item ></Dropdown.Item>
-                            <Dropdown.Item onClick={() => handleSetSortBy('', '')}>Tyhjennä valinta</Dropdown.Item>
-                            <Dropdown.Item onClick={() => handleSetSortBy('popularity.desc', 'Suosituimman mukaan')}>Suosituimman mukaan</Dropdown.Item>
-                            <Dropdown.Item onClick={() => handleSetSortBy('release.asc', 'Vanhimman mukaan')}>Vanhimman mukaan</Dropdown.Item>
-                            <Dropdown.Item onClick={() => handleSetSortBy('release_date.desc', 'Uusiman mukaan')}>Uusiman mukaan</Dropdown.Item>
-                            <Dropdown.Item onClick={() => handleSetSortBy('vote_average.desc', 'Korkein arvosana')}>Korkein arvosana</Dropdown.Item>
-                            <Dropdown.Item onClick={() => handleSetSortBy('vote_average.asc', 'Matalin arvosana')}>Matalin arvosana</Dropdown.Item>
+                            <Dropdown.Item onClick={() => handleSetSortBy('popularity.desc', 'popularity')}>popularity</Dropdown.Item>
+                            <Dropdown.Item onClick={() => handleSetSortBy('vote_average.desc', 'highest rating')}>highest rating</Dropdown.Item>
+                            <Dropdown.Item onClick={() => handleSetSortBy('vote_average.asc', 'lowest rating')}>lowest rating</Dropdown.Item>
+                            <Dropdown.Item onClick={() => handleSetSortBy('release_date.desc', 'newest to oldest')}>newest to oldest</Dropdown.Item>
+                            <Dropdown.Item onClick={() => handleSetSortBy('release.asc', 'oldest to newest')}>oldest to newest</Dropdown.Item>
+                            <Dropdown.Divider />
+                            <Dropdown.Item onClick={() => handleSetSortBy('', '')}>Clear selection</Dropdown.Item>
                         </Dropdown.Menu>
                     </Dropdown>
                 </Col>
@@ -130,7 +151,7 @@ function SearchFilms() {
                 <div className={`tag ${genres.includes('53') ? 'highlighted' : ''}`} onClick={() => handleSetGenre('53')}>Thriller</div>
                 <div className={`tag ${genres.includes('10752') ? 'highlighted' : ''}`} onClick={() => handleSetGenre('10752')}>War</div>
                 <div className={`tag ${genres.includes('37') ? 'highlighted' : ''}`} onClick={() => handleSetGenre('37')}>Western</div>
-                <div className="tag" style={{backgroundColor:'var(--secondary-color)'}}id="genreSlector" onClick={() => handleSetGenre('empty')}>Reset</div>
+                <div className="tag" style={{ backgroundColor: 'var(--secondary-color)' }} id="genreSlector" onClick={() => handleSetGenre('empty')}>Reset</div>
             </div>
 
             <Row style={{ justifyContent: 'center' }}>
@@ -145,6 +166,13 @@ function SearchFilms() {
                     </div>
                 )}
             </Row>
+            {/* If searching movies, hide this, if discovering with genres set this div visible and on click navigate pages.*/}
+            {/* Disable next or previous div if last page or first page. On click increase or decrease current page value and change page*/}
+            <div className={` ${searchInput.trim() !== '' ? 'page-selection-hidden' : 'page-selection'}`}>
+                <div onClick={() => handleDecrementPage()} className={` ${CurrentPage === 1 ? 'select-page-disabled' : 'select-page'}`}> Previous</div>
+                <div className="current-page" >{CurrentPage}</div>
+                <div onClick={() => handleIncrementPage()} className={`${CurrentPage === totalPages ? 'select-page-disabled' : 'select-page '}`}>Next</div>
+            </div>
         </div>
     );
 }
@@ -158,14 +186,15 @@ function MovieCard({ ID, Title, Poster, Rating }) {
             return 'green'
         } else if (Rating <= 7.9 && Rating >= 6.6) {
             return 'lightgreen'
-        } else if (Rating <= 6.5 && Rating >= 5.1) {
+        } else if (Rating <= 6.5 && Rating >= 5.2) {
             return 'yellow';
-        } else if (Rating <= 5 && Rating >= 3.1) {
+        } else if (Rating <= 5.1 && Rating >= 3.1) {
             return 'orange';
         } else if (Rating < 4) {
             return 'red'
         }
     }
+
     //if there is no poster, show this poster
     if (Poster === null) {
         Poster = 'tGX3YyPaYMbjiRwR5PjRsJW7GtM.jpg'
@@ -180,6 +209,8 @@ function MovieCard({ ID, Title, Poster, Rating }) {
             </div>
         </div>
     )
+
+
 }
 
 export { SearchFilms, MovieCard };
