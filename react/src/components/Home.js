e4d4773d182aec297ba3ace5bb7a24ffd62c382a
Antti Alavilo <A2302144@edu.lapinamk.fi>
2023-12-09 13:49:33 +0200

    Upcoming movies to home page
    title.

--- react/src/components/Home.js
+++ react/src/components/Home.js
@@ -16,6 +16,7 @@ function Home() {
       <Container>
         <h1 className="mt-4" style={{ color: '#CA3e47', borderBottom: '1px solid #CA3E47' }}>Welcome</h1>
         <Movies />
+        <UpcomingMovies />
           <h3 className="my-4" style={{ color: '#CA3e47', borderBottom: '1px solid #CA3E47' }}>New reviews</h3>
           <ReviewGrid />
         <Row>
@@ -35,7 +36,7 @@ function Movies({ movie }){
   const navigate = useNavigate();
   const handleMovieClick = (movieID) => {
     // Use navigate to go to the Film component with the clicked movieID
-    navigate(`/home/${movieID}`);
+    navigate(`/Film/${movieID}`);
     };      
     // Add a guard clause to check if similarMovies is defined
     //if (!movie || movie.length === 0) {
@@ -60,7 +61,7 @@ function Movies({ movie }){
     <div>
       <Container>
         <Row>
-          <h3 className="mt-4" style={{ color: '#CA3e47', borderBottom: '1px solid #CA3E47' }}>New</h3>
+          <h3 className="mt-4" style={{ color: '#CA3e47', borderBottom: '1px solid #CA3E47' }}>Trending Movies</h3>
           {films.map(movie =>
             <Col key={movie.movieID} onClick={() => handleMovieClick(movie.movieID)} style={{ width: 'fit-content' }}>
               <MovieCard
@@ -72,15 +73,50 @@ function Movies({ movie }){
             </Col>
           )}
         </Row>
+      </Container>
+    </div>
+  )
+}
+
+function UpcomingMovies({ movie }){
+  const [films, setFilms] = useState([]);
+
+  const navigate = useNavigate();
+  const handleMovieClick = (movieID) => {
+    // Use navigate to go to the Film component with the clicked movieID
+    navigate(`/home/${movieID}`);
+    };      
+    // Add a guard clause to check if similarMovies is defined
+    //if (!movie || movie.length === 0) {
+    //  return;
+    //}
+    // Limit the display to only 5 similar movies
+    //const limitedMovies = movie.slice(0, 5);
+  
+
+  axios.get("https://api.themoviedb.org/3/movie/upcoming?api_key=3972673c7c2bf3c70fc1b5593e956b47")
+                    .then(resp =>
+                        setFilms(resp.data.results.map(upcomingMovie => ({
+                            Rating: upcomingMovie.vote_average,
+                            movieID: upcomingMovie.id,
+                            Title: upcomingMovie.title,
+                            Poster: upcomingMovie.poster_path
+                        }))))
+                    .catch(error => {
+                        console.error('Error fetching data:', error);
+                    });
+  return(
+    <div>
+      <Container>
         <Row>
-          <h3 className="mt-4" style={{ color: '#CA3e47', borderBottom: '1px solid #CA3E47' }}>Most popular</h3>
-          {films.map(movie =>
-            <Col key={movie.movieID} onClick={() => handleMovieClick(movie.movieID)} style={{ width: 'fit-content' }}>
+          <h3 className="mt-4" style={{ color: '#CA3e47', borderBottom: '1px solid #CA3E47' }}>Upcoming Movies</h3>
+          {films.map(upcomingMovie =>
+            <Col key={upcomingMovie.movieID} onClick={() => handleMovieClick(upcomingMovie.movieID)} style={{ width: 'fit-content' }}>
               <MovieCard
-                ID={movie.movieID}
-                Title={movie.Title}
-                Poster={`https://image.tmdb.org/t/p/original${movie.Poster}`}
-                Rating={movie.Rating}
+                ID={upcomingMovie.movieID}
+                Title={upcomingMovie.Title}
+                Poster={`https://image.tmdb.org/t/p/original${upcomingMovie.Poster}`}
+                Rating={upcomingMovie.Rating}
               />      
             </Col>  
           )}
